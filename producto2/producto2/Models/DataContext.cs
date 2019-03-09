ce87d549951f87d99a65cb58e4f54ab47a593013
pcloayza <paulocesarloayza@upsa.edu.bo>
2019-03-09 13:53:50 -0400

    modelo

---
+++ producto2/producto2/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace producto2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection") {  }
+
+        public System.Data.Entity.DbSet<producto2.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ producto2/producto2/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace producto2.Models
+{
+    public class Producto
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
